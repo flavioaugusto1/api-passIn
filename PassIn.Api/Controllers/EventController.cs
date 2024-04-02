437ffbb2e6ce8513cce49d0b46c9e17a0359204b
Flávio <f.augustosdn@gmail.com>
2024-04-01 22:08:24 -0300

    Add project files.

---
+++ PassIn.Api/Controllers/EventController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PassIn.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class EventController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Register()
+    {
+
+        return Created();
+    }
+}
